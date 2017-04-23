9efce0c764f429653896f4fbd3c8114587ff3dce
Andrea Kopecna <a.kopecna@seznam.cz>
2017-04-23 21:37:00 +0200

    Pridana funkce pro vipis kratkych zvirat (ukol 1)

--- 08/ukol_0.py
+++ 08/ukol_0.py
@@ -1,1 +1,8 @@
-domaci_zvirtata = ['pes', 'kocka', 'kralik', 'had']
+domaci_zvirata = ['pes', 'kocka', 'kralik', 'had']
+
+def vypis_kratka_zvirata(zvirata):
+    for zvire in zvirata:
+        if len(zvire) < 5:
+            print(zvire)
+
+vypis_kratka_zvirata(domaci_zvirata)
