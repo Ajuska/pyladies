b1c14b803f1e84d044b7e8a8b701472fdb7aa789
Andrea Kopecna <a.kopecna@seznam.cz>
2017-04-24 16:59:56 +0200

    Dopsani ukolu 1,2,3

--- 08/ukol_0.py
+++ 08/ukol_0.py
@@ -5,4 +5,24 @@ def vypis_kratka_zvirata(zvirata):
         if len(zvire) < 5:
             print(zvire)
 
+
+def vypis_k_zvirata(zvirata):
+    for zvire in zvirata:
+        if zvire[0] == 'k':
+            print(zvire)
+
+
+def overit_seznam_zvirat(zvirata):
+    if zvirata in domaci_zvirata:
+        print('Ano')
+        return True
+    else:
+        print('Ne')
+        return False
+
+
 vypis_kratka_zvirata(domaci_zvirata)
+vypis_k_zvirata(domaci_zvirata)
+
+otazka_pro_overeni = input('Zadej jemno zvirete pro overeni v seznamu:')
+overit_seznam_zvirat(otazka_pro_overeni)
