c307046e6875b418fb4eb71b48092307ce042609
Andrea Kopecna <a.kopecna@seznam.cz>
2017-05-29 20:00:12 +0200

    Uz jde prohrat

--- 12/hra.py
+++ 12/hra.py
@@ -1,48 +1,75 @@
 import pyglet
 import math
 from pyglet import gl
+from random import randrange, uniform
 window = pyglet.window.Window()
 
 ACCELERATION = 100
 ROTATION_SPEED = 200
 
-spaceship_picture = pyglet.image.load("/home/aja/pyladies/12/obrazky/PNG/playerShip1_blue.png")
-spaceship_picture.anchor_x = spaceship_picture.width // 2
-spaceship_picture.anchor_y = spaceship_picture.height // 2
+ASTEROID_SPEED = 200
+ASTEROID_ROTATION_SPEED = 100
+
+
+def load_image(filename):
+    picture = pyglet.image.load(filename)
+    picture.anchor_x = picture.width // 2
+    picture.anchor_y = picture.height // 2
+    return picture
+
+
+spaceship_picture = load_image("/home/aja/pyladies/12/obrazky/PNG/playerShip1_blue.png")
+asteroid_picture = load_image("/home/aja/pyladies/12/obrazky/PNG/Meteors/meteorBrown_big1.png")
 batch = pyglet.graphics.Batch()
 
 pressed_keys = set()
 
+
+def draw_circle(x, y, radius):
+  iterations = 20
+  s = math.sin(2*math.pi / iterations)
+  c = math.cos(2*math.pi / iterations)
+
+  dx, dy = radius, 0
+
+  gl.glBegin(gl.GL_LINE_STRIP)
+  for i in range(iterations+1):
+      gl.glVertex2f(x+dx, y+dy)
+      dx, dy = (dx*c - dy*s), (dy*c + dx*s)
+  gl.glEnd()
+
+
+def distance(a, b, wrap_size):
+    """Distance in one direction (x or y)"""
+    result = abs(a - b)
+    if result > wrap_size / 2:
+        result = wrap_size - result
+    return result
+
+def overlaps(a, b):
+    """Returns true iff two space objects overlap"""
+    distance_squared = (distance(a.x, b.x, window.width) ** 2 +
+                        distance(a.y, b.y, window.height) ** 2)
+    max_distance_squared = (a.radius + b.radius) ** 2
+    return distance_squared < max_distance_squared
+
 class SpaceObject:
     def __init__(self, window):
         self.x = 0
-        self.x = 0
+        self.y = 0
         self.x_speed = 0
         self.y_speed = 0
         self.rotation = 0
+        self.rotation_speed = 0
+        self.radius = 35
         self.window = window
-
-class Spaceship(SpaceObject):
-    def __init__(self, window):
-        super().__init__(window)
-        self.x = window.width / 2
-        self.y = window.height / 2
-        self.sprite = pyglet.sprite.Sprite(spaceship_picture, batch=batch)
 
 
     def tick(self, t):
-        rotation_radians = math.radians(self.rotation)
-        if pyglet.window.key.UP in pressed_keys:
-            self.x_speed = self.x_speed + math.cos(rotation_radians) * ACCELERATION * t
-            self.y_speed = self.y_speed + math.sin(rotation_radians) * ACCELERATION * t
-        if pyglet.window.key.RIGHT in pressed_keys:
-            self.rotation = self.rotation - ROTATION_SPEED * t
-        if pyglet.window.key.LEFT in pressed_keys:
-            self.rotation = self.rotation + ROTATION_SPEED * t
-
+        #Movement
         self.x = self.x + self.x_speed * t
         self.y = self.y + self.y_speed * t
-
+        self.rotation = self.rotation + self.rotation_speed * t        #Wrap at the edges of the screen
         while self.x > self.window.width:
             self.x = self.x - self.window.width
         while self.y > self.window.height:
@@ -51,27 +78,84 @@ class Spaceship(SpaceObject):
             self.x = self.x + self.window.width
         while self.y < 0:
             self.y = self.y + self.window.height
-
+        #Update the sprite
         self.sprite.x = self.x
         self.sprite.y = self.y
         self.sprite.rotation = 90 - self.rotation
 
+    def delete(self):
+        objects.remove(self)
+        self.sprite.delete()
+
+class Asteroid(SpaceObject):
+    def __init__(self, window):
+        super().__init__(window)
+        self.sprite = pyglet.sprite.Sprite(asteroid_picture, batch=batch)
+
+        self.x_speed = uniform(-ASTEROID_SPEED, ASTEROID_SPEED)
+        self.y_speed = uniform(-ASTEROID_SPEED, ASTEROID_SPEED)
+        self.radius = 40
+
+        if randrange(2) == 0:
+            self.x = uniform(0, window.width)
+        else:
+            self.y = uniform(0, window.height)
+
+        self.rotation_speed = uniform(-ASTEROID_ROTATION_SPEED,
+                                        ASTEROID_ROTATION_SPEED)
+
+class Spaceship(SpaceObject):
+    def __init__(self, window):
+        super().__init__(window)
+        self.x = window.width / 2
+        self.y = window.height / 2
+        self.sprite = pyglet.sprite.Sprite(spaceship_picture, batch=batch)
+
+    def tick(self, t):
+        #Heading in radlans
+        rotation_radians = math.radians(self.rotation)
+        #Keybord control - speed up or turn
+        if pyglet.window.key.UP in pressed_keys:
+            self.x_speed = self.x_speed + math.cos(rotation_radians) * ACCELERATION * t
+            self.y_speed = self.y_speed + math.sin(rotation_radians) * ACCELERATION * t
+        if pyglet.window.key.RIGHT in pressed_keys:
+            self.rotation = self.rotation - ROTATION_SPEED * t
+        if pyglet.window.key.LEFT in pressed_keys:
+            self.rotation = self.rotation + ROTATION_SPEED * t
+        #Slow down gradually
+        self.x_speed = self.x_speed / 1.5 ** t
+        self.y_speed = self.y_speed  / 1.5 ** t
+
+        super().tick(t)
+
+        for obj in list(objects):
+            if obj != self:
+                if overlaps(obj, self):
+                    self.delete()
+                    #raise Exeption('Game over!')
+
+#Create initial objects
 objects = []
 for i in range(1):
     spaceship = Spaceship(window)
     spaceship.rotation = i * 10
     objects.append(spaceship)
 
+asteroid = Asteroid(window)
+objects.append(asteroid)
+
+
 def on_draw():
     window.clear()
-
     for x in -window.width, 0, window.width:
         for y in -window.height, 0, window.height:
             gl.glPushMatrix()
             gl.glTranslatef(x, y, 0)
-            for obj in objects:
-                batch.draw()
+            batch.draw()
             gl.glPopMatrix()
+
+    for obj in objects:
+        draw_circle(obj.x, obj.y, obj.radius)
 
 def tick(t):
     for obj in objects:
